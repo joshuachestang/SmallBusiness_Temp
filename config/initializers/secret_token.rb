d07d8357534510b2f917ceb5bb65c3e62bc241f7
kfn8dkodemonkey <kaffeinatedkodemonkey@gmail.com>
2013-11-14 20:40:17 -0700

    now app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmallBusinessTemp::Application.config.secret_key_base = '6be4fc02512d3955ff744acf3e5e2d7974e39793528fef1e093f1214a11325dda10b46c6bf0f323c5bf2475878ae63f6d6ceedeef7676f25101a854a6e1adc65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmallBusinessTemp::Application.config.session_store :cookie_store, key: '_SmallBusiness_Temp_session'
